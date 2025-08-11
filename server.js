ba44f240f68a4dc3538599760cef9154dbd21752
Dev Joshi <devjoshi.inquire1@gmail.com>
2025-08-11 12:55:13 +0530

    - Fixed allowedOrigins link

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000
 
 const allowedOrigins = [
   'http://localhost:5173',  // local dev
-  'https://chantflow-fe.onrender.com/'  // production
+  'https://chantflow-fe.onrender.com'  // production
 ];
 
 app.use(cors({
